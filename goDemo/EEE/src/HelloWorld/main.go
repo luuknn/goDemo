9c6a9dd17a9a8da99ee0161501401e1199d54efc
jianlingshih <jianling.shih@gmail.com>
2016-11-09 00:17:45 +0800

    test123

--- goDemo/EEE/src/HelloWorld/main.go
+++ goDemo/EEE/src/HelloWorld/main.go
@@ -14,7 +14,7 @@ func ready(w string, sec int64) {
 func main() {
 	go ready("Tee", 2)
 	go ready("Coffee", 1)
-	fmt.Println("T'm waiting")
+	fmt.Println("T'm waiting~")
 	sec := 1
 	secs := time.Duration(sec) * time.Second
 	time.Sleep(5 * secs)
