8349d8df0eff629876cfe51dbc34282186ff760e
jianlingshih <jianling.shih@gmail.com>
2016-11-23 23:48:03 +0800

    2.8 总结

---
+++ goDemo/EEE/src/2.8 总结/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+//这一章 我们主要介绍了 Go 语言的一些语法 通过语法我们可以发现Go是多么简单
+//只有二十五个关键字 让我们再来回顾一下 这些关键字都是用来干什么的
+//break default func interface select case defer go map struct
+//chan else goto package switch const fallthrough if range type
+//continye for import retuen var
+
+//var 和 const 参考 2.2Go语言基础里面的变量 和常量声明
+//package和import 已经有过短暂的接触
+//func 用于定义函数和方法
+//return 用于从函数返回
+//defer 用于类似析构函数
+//go用于并行
+//select 用于选择不同类型的tongxun
+//interface 用于定义接口
+//struct用于定义抽象数据类型
+//break case continue for fallthrough else if switch goto default
+//chan 用于channel 通讯
+//type 用于声明自定义类型
+//map 用于声明 map类型数据
+//range用于读取slice map channel 数据
+
+//上面25个关键字记住了 差不多 Go入门..
