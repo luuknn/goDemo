09eb2c492c6da2fc890d83ede71f7ee6ae013094
jianlingshih <jianling.shih@gmail.com>
2017-05-18 07:48:36 +0800

    给web上传文件 添加注解

--- goDemo/EEE/src/0zzupload/new_file.go
+++ goDemo/EEE/src/0zzupload/new_file.go
@@ -18,11 +18,15 @@ func upload(w http.ResponseWriter, r *http.Request) {
 		checkErr(err)
 		t.Execute(w, nil)
 	} else {
+		//解析form中file上传的名字
 		file, handle, err := r.FormFile("file")
 		checkErr(err)
+		//打开 已只读 文件不存在创建 方式打开 要存放的路径
 		f, err := os.OpenFile("test/"+handle.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		//文件拷贝
 		io.Copy(f, file)
 		checkErr(err)
+		//关闭对应打开的文件
 		defer f.Close()
 		defer file.Close()
 		fmt.Println("upload success")
@@ -37,7 +41,9 @@ func checkErr(err error) {
 
 }
 func main() {
+	//绑定路由 如果访问 /upload 调用Handler方法
 	http.HandleFunc("/upload", upload)
+	//使用tcp协议监听 8888端口号
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
 		log.Fatal("listenAndServe: ", err)
