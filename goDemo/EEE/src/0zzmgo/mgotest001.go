00827626584604a036bee9bfe67ab61b085db00a
jianlingshih <jianling.shih@gmail.com>
2017-07-20 07:10:18 +0800

    mgo 初探001

--- goDemo/EEE/src/0zzmgo/mgotest001.go
+++ goDemo/EEE/src/0zzmgo/mgotest001.go
@@ -4,8 +4,45 @@ package main
 //	"fmt"
 //	"gopkg.in/mgo.v2"
 //	"gopkg.in/mgo.v2/bson"
-//	"log"
+//	//	"log"
 //)
+//
+//type Mail struct {
+//	Id    bson.ObjectId "_id"
+//	Name  string
+//	Email string
+//}
+//
+//func main() {
+//	//连接数据库
+//	session, err := mgo.Dial("106.14.181.48")
+//	if err != nil {
+//		panic(err)
+//	}
+//	defer session.Close()
+//	//获取数据库 获取集合
+//	c := session.DB("mydb").C("mail")
+//	//存储数据
+//	m1 := Mail{bson.NewObjectId(), "user1", "user1@gmail.com"}
+//	m2 := Mail{bson.NewObjectId(), "user2", "user2@gmail.com"}
+//	m3 := Mail{bson.NewObjectId(), "user2", "user3@gmail.com"}
+//	err = c.Insert(&m1, &m2, &m3)
+//	if err != nil {
+//		panic(err)
+//	}
+//	//读取数据
+//	ms := []Mail{}
+//	err = c.Find(bson.M{"name": "user2"}).All(&ms)
+//	if err != nil {
+//		panic(err)
+//	}
+//	//显示数据
+//	for i, m := range ms {
+//		fmt.Printf("%s,%d,%s\n", m.Id.Hex(), i, m.Email)
+//	}
+//
+//}
+
 //
 //type Person struct {
 //	Name  string
